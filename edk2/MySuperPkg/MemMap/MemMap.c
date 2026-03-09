0484f3e458334c6979a2ffb91879d4924ac42c49
Maksim Dolgov <maksim.dolgov.06@mail.ru>
2026-03-09 11:47:20 +0300

    Полное объяснение кода

--- edk2/MySuperPkg/MemMap/MemMap.c
+++ edk2/MySuperPkg/MemMap/MemMap.c
@@ -54,7 +54,20 @@ GetUefiMemoryMap(
   UINTN dscr_size = 0;                // Размер одного дескриптора
   UINT32 dscr_vers = 0;               // Версия структуры дискриптора памяти
 
+  /*
+  Бесконечный цикл получения карты памяти. Мы из него выходим когда:
+  1) Успешно получили карту памяти
+  2) Получили ошибку отличную от EFI_BUFFER_TOO_SMALL 
+  */
   while (1) {
+
+    /*
+    Пытаемся получить карту памяти.
+    При первом вызове mmap = NULL и mmap_size = 0
+    Это означает, что пока не дается буфер а просто говорится какого размера он нужен
+    В этом случае UEFI возвращает EFI_BUFFER_TOO_SMALL
+    а в mmap_size записывается нужный размер буфера 
+    */
     s = gBS->GetMemoryMap(
               &mmap_size,
               mmap,
@@ -62,35 +75,49 @@ GetUefiMemoryMap(
               &dscr_size,
               &dscr_vers
             );
-
+    
+    /*
+    Если вернулся EFI_SUCCESS, значит карта памяти успешно получена
+    */
     if (s == EFI_SUCCESS) {
-      // Успешно получили карту памяти
-      *OutMemoryMap = mmap;
-      *OutMemoryMapSize = mmap_size;
-      *OutMapKey = mmap_key;
-      *OutDescriptorSize = dscr_size;
-      *OutDescriptorVersion = dscr_vers;
-      return EFI_SUCCESS;
+      *OutMemoryMap = mmap;               // Возвращаем указатель на буфер с картой памяти
+      *OutMemoryMapSize = mmap_size;      // Общий размер карты памяти в байтах
+      *OutMapKey = mmap_key;              // Ключ карты памяти
+      *OutDescriptorSize = dscr_size;     // Размер одного дескриптора
+      *OutDescriptorVersion = dscr_vers;  // Версия дескриптора 
+      return EFI_SUCCESS;                 // Завершаем функцию с успехом
     }
 
-    if (s != EFI_BUFFER_TOO_SMALL) {
-      // Какая-то другая ошибка
+    /*
+    Какая-то другая ошибка не EFI_BUFFER_TOO_SMALL
+    Это не ожидаемая ситуация, а действительная ошибка
+    */
+    if (s != EFI_BUFFER_TOO_SMALL) { 
       if (mmap != NULL) {
-        gBS->FreePool(mmap);
-      }
+        gBS->FreePool(mmap);              // Если до этого буфер выделялся, то освобождаем его,
+      }                                   // чтобы не было утечек памяти
       return s;
     }
 
-    // Если буфер маленький — освобождаем старый (если был)
-    if (mmap != NULL) {
-      gBS->FreePool(mmap);
-      mmap = NULL;
-    }
+    /*
+    Если мы дошли сюда, значит у нас s == EFI_BUFFER_TOO_SMALL
+    Буфер слишком маленький его нужно выделить/увеличить
+    */
+    if (mmap != NULL) {                   // Если буфер уже был выделен на прошлой итерации
+      gBS->FreePool(mmap);                // освобождаем его и будем выделять заново
+      mmap = NULL;                        // Карта памяти могла увеличиться между вызовами и
+    }                                     // требуемый размер мог увеличиться 
 
     // Добавляем небольшой запас, потому что карта памяти
     // может измениться между вызовами
     mmap_size += 2 * dscr_size;
 
+    /* 
+    Выделяем буфер под карту памяти
+    EfiBootServicesData — тип памяти,
+    mmap_size — сколько байт нужно,
+    &mmap — сюда записывается адрес выделенной памяти.
+    */
     s = gBS->AllocatePool(EfiBootServicesData, mmap_size, (VOID **)&mmap);
     if (EFI_ERROR(s)) {
       return s;
@@ -98,7 +125,10 @@ GetUefiMemoryMap(
   }
 }
 
-
+/*
+Функция для преобразование стандартной карты памяти UEFI 
+в нашу собственную карту памяти
+*/
 EFI_STATUS
 ConvertMemoryMap(
   EFI_MEMORY_DESCRIPTOR *mmap,
@@ -108,27 +138,37 @@ ConvertMemoryMap(
 )
 {
   EFI_STATUS s;
-  UINT64 entries_count = mmap_size / dscr_size;
+  UINT64 entries_count = mmap_size / dscr_size;                              // Вычисляем количество записей в карте памяти
+  UINT64 new_mmap_size = sizeof(UINT64) + entries_count * sizeof(MmapEntry); // Вычисляем сколько памяти нужно под нашу новую карту памяти
+  Mmap *new_mmap = NULL;                                                     // Указатель на новую карту памяти
 
-  UINT64 new_mmap_size = sizeof(UINT64) + entries_count * sizeof(MmapEntry);
-  Mmap *new_mmap = NULL;
-
+  // Выделяем память под нашу новую карту памяти
   s = gBS->AllocatePool(EfiBootServicesData, new_mmap_size, (VOID **)&new_mmap);
   if (EFI_ERROR(s)) {
     return s;
   }
 
+  // Записываем сколько записей будет в новой карте памяти
   new_mmap->size = entries_count;
 
+  // Проходим по всем дескрипторам исходной карты памяти UEFI 
   for (UINT64 i = 0; i < new_mmap->size; ++i) {
-    UINTN offset = dscr_size * i;
+    UINTN offset = dscr_size * i;                              // Вычисляем смещение до i-ого дескриптора в байтах
 
+    /*
+    Получаем указатель на текущий дескриптор памяти
+    Здесь mmap приводится к UINT8*
+    чтобы можно было двигаться по памяти на число байт.
+    Потом результат снова приводится к EFI_MEMORY_DESCRIPTOR*.
+    */
     EFI_MEMORY_DESCRIPTOR *d =
         (EFI_MEMORY_DESCRIPTOR *)((UINT8 *)mmap + offset);
 
-    MmapEntryType t = MmapEntryType_NotAvailable;
+    MmapEntryType t = MmapEntryType_NotAvailable;               // Считаем по умолчанию, что память не доступна
 
     switch (d->Type) {
+      // Смотрим на тип памяти котоый вернул UEFI
+      // Эти типы считаем доступными
       case EfiLoaderCode:
       case EfiLoaderData:
       case EfiBootServicesCode:
@@ -142,14 +182,12 @@ ConvertMemoryMap(
         break;
     }
 
-    new_mmap->entries[i].addr = d->PhysicalStart;
-
-    new_mmap->entries[i].size = d->NumberOfPages * SIZE_4KB;
-
-    new_mmap->entries[i].type = t;
+    new_mmap->entries[i].addr = d->PhysicalStart;             // Начальный физ адрес участка памяти
+    new_mmap->entries[i].size = d->NumberOfPages * SIZE_4KB;  // Размер участка в байтах (число страниц * 4096)
+    new_mmap->entries[i].type = t;                            // Наш тип (доступен/нелоступен)
   }
 
-  *OutNewMap = new_mmap;
+  *OutNewMap = new_mmap;                                      // Возвращаем наружу указатель на новую перепакованную карту памяти
   return EFI_SUCCESS;
 }
 
@@ -161,21 +199,26 @@ PrintMemoryMap(
   Mmap *mmap
 )
 {
-  Print(L"Memory map entries count: %lu\r\n", mmap->size);
+  Print(L"Memory map entries count: %lu\r\n", mmap->size);                      
   Print(L"---------------------------------------------------------\r\n");
   Print(L"StartAddr         Size              Type\r\n");
   Print(L"---------------------------------------------------------\r\n");
 
+  // Проходим по всем нашим записям карты памяти
   for (UINT64 i = 0; i < mmap->size; ++i) {
     Print(
-      L"%016lx %016lx %d\r\n",
-      mmap->entries[i].addr,
-      mmap->entries[i].size,
-      mmap->entries[i].type
+      L"%016lx %016lx %d\r\n", 
+      mmap->entries[i].addr,          // Начальный адрес участка памяти
+      mmap->entries[i].size,          // Размер учатка памяти
+      mmap->entries[i].type           // Тип (0 = недоступен, 1 = доступен)
     );
   }
 }
 
+
+/* 
+Точка входа в UEFI - приложение 
+*/
 EFI_STATUS
 EFIAPI
 UefiMain(
@@ -191,7 +234,7 @@ UefiMain(
   UINTN dscr_size = 0;
   UINT32 dscr_vers = 0;
 
-  Mmap *new_mmap = NULL;
+  Mmap *new_mmap = NULL;                // Наша перепакованная карта памяти 
 
   // Получаем карту памяти UEFI в цикле
   s = GetUefiMemoryMap(
@@ -202,14 +245,15 @@ UefiMain(
         &dscr_vers
       );
 
+  // Если не удалось получить карту памяти выводим ошибку
   if (EFI_ERROR(s)) {
     Print(L"GetUefiMemoryMap error: %r\r\n", s);
     return s;
   }
 
-  Print(L"Memory map retrieved successfully\r\n");
-  Print(L"mmap = %p\r\n", mmap);
-  Print(L"mmap_size = %lu, descriptor_size = %lu\r\n", mmap_size, dscr_size);
+  Print(L"Memory map retrieved successfully\r\n");                            // Сообщаем, что карта получена успешно
+  Print(L"mmap = %p\r\n", mmap);                                              // Печатаем адрес буфера, где лежит карта памяти UEFI
+  Print(L"mmap_size = %lu, descriptor_size = %lu\r\n", mmap_size, dscr_size); // Печатаем общий размер карты памяти и размер одного дескриптора
 
   // Перепаковываем карту памяти
   s = ConvertMemoryMap(mmap, mmap_size, dscr_size, &new_mmap);
@@ -219,14 +263,10 @@ UefiMain(
     return s;
   }
 
-  Print(L"New mmap = %p\r\n", new_mmap);
-
-  // Печатаем новую карту памяти
-  PrintMemoryMap(new_mmap);
-
-  // Освобождаем ресурсы
-  gBS->FreePool(new_mmap);
-  gBS->FreePool(mmap);
+  Print(L"New mmap = %p\r\n", new_mmap);  // Печатаем адрес нашей новой карты памяти
+  PrintMemoryMap(new_mmap);               // Печатаем новую, перепакованную карту памяти на экран
+  gBS->FreePool(new_mmap);                // Освобождаем память выделенную под новую карту памяти
+  gBS->FreePool(mmap);                    // Освобождаем память выделенную под исходную карту памяти UEFI
 
   return EFI_SUCCESS;
 }
